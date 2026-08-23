c604eb3590cef3809c31d1b7d450b9a4339f9dc5
NguyenDung <mdung0802@gmail.com>
2026-08-23 16:56:57 +0700

    Yocto start

---
+++ bootlin/Yocto-Project/README.md
@@ -0,0 +1,4 @@
+# Menu
+
+# Introduction to Embedded Linux
+## Simplified Linux system architecture 
---
+++ toolCPU/checkPerf.sh
@@ -0,0 +1,2 @@
+ cat /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+ cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
